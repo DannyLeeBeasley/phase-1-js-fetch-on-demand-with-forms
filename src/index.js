1a89522caf5f461e5ce5e213d62d09e117f1384c
Dan Beasley <dannyleebeasley@gmail.com>
2021-05-06 08:55:24 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    const inputBox = document.querySelector('input#searchByID');
+    inputForm.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        fetch(`http://localhost:3000/movies/${inputBox.value}`)
+        .then(res => res.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+
+    })
   
 }
 
